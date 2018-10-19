c9a18c32235c6505117e6b7cbc4664e0c7408c4b
Don Hamilton III <donhamiltoniii@gmail.com>
2018-10-19 19:40:21 -0400

    Add project buil

---
+++ src/main/java/org/wecancodeit/jsjestpracticeexercise/JsJestPracticeExerciseApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.jsjestpracticeexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsJestPracticeExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsJestPracticeExerciseApplication.class, args);
+	}
+}
